8ed43032b66f1b3d475ccc11b52ae115ad3c39ca
PHNX-MOD <modith.hadya@gmail.com>
2023-08-15 12:42:24 +0200

    change in main file

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
 install.packages("RSQLite")
 library(RSQLite)
-con <- dbConnect(SQLite(), dbname = "porsche")
-query <- 'SELECT * FROM demo;'
-data <- dbGetQuery(con, query)
 
-sqldbDisconnect(con)
+con <- dbConnect(RSQLite::SQLite(), "mydatabase.db")
+result <- dbGetQuery(con, "SELECT * FROM cbb")
+dbDisconnect(con)
+
+
 
 #changes to be made from replt

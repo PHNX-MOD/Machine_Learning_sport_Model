28ce78debdb1cd31402017c315a7fac0a761542d
PHNX-MOD <modith.hadya@gmail.com>
2023-09-26 16:26:54 +0200

    change in db file

--- main.R
+++ main.R
@@ -6,29 +6,6 @@ result <- dbGetQuery(con, "SELECT * FROM box_scores")
 dbDisconnect(con)
 
 
-folder <- "."
 
-#Get a list of the CSV files in the folder
-csv_files <- list.files('.', pattern = "\\.csv$")
-
-
-for (csv_file in csv_files) {
-  df <- read.csv(file.path(folder, csv_file))
-  
-  table_name <- gsub("\\.csv$", "", csv_file)
-  
-  dbWriteTable(con, table_name, df)
-}
-
-
-
-df <- read.csv('box_scores.csv')
-
-dbWriteTable(con, 'box_scores' , df)
-
-gsub("\\.csv$", "", "box_scores.csv")
-
-
-#try this 
 
 

7d68fa115ff4b6f736ef4375e1f7d8b4656430c7
Modith Hadya <40522163+PHNX-MOD@users.noreply.github.com>
2023-09-28 12:49:47 -0700

    data preprocessing

--- main.R
+++ main.R
@@ -17,8 +17,33 @@ df_fixture_information <- dbGetQuery(con, "SELECT * FROM fixture_information")
 df_test_fixtures <- dbGetQuery(con, "SELECT * FROM test_fixtures")
 df_test_fixtures_actuals <- dbGetQuery(con, "SELECT * FROM test_fixtures_actuals")
 
+
+
+#data Preprocessing 
+
 skim(df_box_scores)
 skim(df_fixture_information)
 skim(df_test_fixtures)
 skim(df_test_fixtures_actuals)
+
+# box scores:  split into test and train
+
+df_box_scores_split <- initial_split(df_box_scores)
+df_box_scores_train <- training(df_box_scores_split)
+df_box_scores_test <- testing(df_box_scores_split)
+
+skim(df_box_scores_train)
+
+
+# df_test_fixtures_actuals : split into test and train
+
+df_test_fixtures_actuals_split <- initial_split(df_test_fixtures_actuals)
+df_test_fixtures_actuals_train <- training(df_test_fixtures_actuals_split)
+df_test_fixtures_actuals_test <- testing(df_test_fixtures_actuals_split)
+
+#checking missing values
+skim(df_test_fixtures_actuals_train)
+any(is.na(df_test_fixtures_actuals_train))
+colSums(is.na(df_test_fixtures_actuals_train)) #TeamHandicap NA values 
+
 

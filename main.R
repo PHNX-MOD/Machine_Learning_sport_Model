2d13bdcf28f469ae10e2efbe063cab4fb5a2ca4a
PHNX-MOD <modith.hadya@gmail.com>
2023-09-28 12:57:21 +0200

    data preprocessing

--- main.R
+++ main.R
@@ -1,9 +1,17 @@
+library(RSQLite)
 
 con <- dbConnect(RSQLite::SQLite(), "mydatabase.db")
-result <- dbGetQuery(con, "SELECT * FROM box_scores")
 dbDisconnect(con)
 
 
+#Data Preparation
+# Load the datasets (box_scores.csv, fixture_information.csv, 
+# test_fixtures.csv) into R using functions like read.csv or 
+# any suitable function based on the file format.
 
-
+#connect to DB and pull the datasets 
+df_box_scores <- dbGetQuery(con, "SELECT * FROM box_scores")
+df_fixture_information <- dbGetQuery(con, "SELECT * FROM fixture_information")
+df_test_fixtures <- dbGetQuery(con, "SELECT * FROM test_fixtures")
+df_test_fixtures_actuals <- dbGetQuery(con, "SELECT * FROM test_fixtures_actuals")
 

4362141e1f2949434e0f5d6f2d79b79d72b23ba0
Modith Hadya <40522163+PHNX-MOD@users.noreply.github.com>
2023-10-03 04:59:26 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,32 @@
+qurt1 <- "
+WITH TeamAvTeamB AS (
+    SELECT
+        FixtureKey,
+        SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12) AS TeamAvTeamB
+    FROM
+        box_scores
+)
 SELECT
-    SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12) AS TeamAvTeamB,
-    SUBSTR(SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12), 1, INSTR(SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12), 'v') - 1) AS TeamName,
-    Team, X2PM,  X2PA, X3PM, X3PA, FTM, FTA, ORB,  DRB, AST, STL, BLK,TOV, PF
+    CASE
+        WHEN Team = 1 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)
+        WHEN Team = 2 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
+        ELSE NULL -- Handle other cases if needed
+    END AS TeamName,
+    Team,
+    X2PM,
+    X2PA,
+    X3PM,
+    X3PA,
+    FTM,
+    FTA,
+    ORB,
+    DRB,
+    AST,
+    STL,
+    BLK,
+    TOV,
+    PF
 FROM
-    box_scores;
+    TeamAvTeamB
+    JOIN box_scores ON TeamAvTeamB.FixtureKey = box_scores.FixtureKey;"
+

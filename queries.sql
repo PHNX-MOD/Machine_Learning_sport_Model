20f3ad0ae163b957e766049d4dca19ba32391d30
Modith Hadya <40522163+PHNX-MOD@users.noreply.github.com>
2023-10-05 06:45:48 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,28 +5,18 @@ WITH TeamAvTeamB AS (
     FROM
         box_scores
 )
-SELECT
+SELECT DISTINCT
     TeamAvTeamB.FixtureKey AS FixtureKey, 
     TRIM(CASE
             WHEN Team = 1 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)
             WHEN Team = 2 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
             ELSE NULL -- Handle other cases if needed
         END) AS TeamName,
-    Team,
-    X2PM,
-    X2PA,
-    X3PM,
-    X3PA,
-    FTM,
-    FTA,
-    ORB,
-    DRB,
-    AST,
-    STL,
-    BLK,
-    TOV,
-    PF
+    Team, X2PM, X2PA, X3PM, X3PA, FTM, FTA, ORB, DRB, AST, STL, BLK,TOV,
+    PF,
+    ROUND((CAST(X2PM AS REAL) + CAST(X3PM AS REAL)) / (CAST(X2PA AS REAL) + CAST(X3PA AS REAL))*100,2) AS 'FG%',
+    ROUND((CAST(X2PM AS REAL) / CAST(X3PA AS REAL))*100,2)  AS'3P%',
+    ROUND((CAST(FTM AS REAL) / CAST(FTA AS REAL))*100,2)  AS'FT%'
 FROM
     TeamAvTeamB
     JOIN box_scores ON TeamAvTeamB.FixtureKey = box_scores.FixtureKey;
-

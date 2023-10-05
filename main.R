0352d3a91b25002eaa9941022d070b30c62a07d8
Modith Hadya <40522163+PHNX-MOD@users.noreply.github.com>
2023-10-05 05:28:27 -0700

    added goals/steps in R

--- main.R
+++ main.R
@@ -36,76 +36,70 @@ df_fixture_information <- dbGetQuery(con, "SELECT * FROM fixture_information")
 df_test_fixtures <- dbGetQuery(con, "SELECT * FROM test_fixtures")
 df_test_fixtures_actuals <- dbGetQuery(con, "SELECT * FROM test_fixtures_actuals")
 
-skim(df_box_scores)
-skim(df_fixture_information)
-skim(df_test_fixtures)
-skim(df_test_fixtures_actuals)
 
-FixtureKey <- "LIPSCO v A PEAY 14-Jan-2023"
-
-splitfun = unlist(strsplit(df_box_scores$FixtureKey[1], " "))
-team_names <- paste(unlist(strsplit(df_box_scores$FixtureKey[1], " "))[1:4], collapse = " ")
-date <- unlist(strsplit(df_box_scores$FixtureKey[1], " "))[5]
-
-
-# ====================Method one splitString ===============================================
-
-dfBoxScores <-df_box_scores %>%rowwise()%>%
-  mutate(
-    SplittedString = strsplit(FixtureKey, " "),
-    TeamAvTeamB = paste(unlist(SplittedString[-length(SplittedString)]), collapse = " ")
-  )%>%
-  mutate(TeamName = strsplit(TeamAvTeamB, "(?<!V)v", perl = TRUE)[[1]][Team])%>%
-  select(!TeamAvTeamB)%>%
-  select(TeamName,FixtureKey,Team,X2PM,X2PA,X3PM,X3PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF)
-
-
-dfBoxScores <-dfBoxScores%>%group_by(TeamName)%>%
-  summarise(X2PA = mean(X2PA))
-
-
-# ====================Methods two splitString ============================================
-
-
-
-dfBoxScores2 <- df_box_scores %>%rowwise()%>%
-  mutate(TeamAvTeamB = sub(" \\d{2}-\\w{3}-\\d{4}$", "", FixtureKey))%>%
-  mutate(TeamName = strsplit(TeamAvTeamB, "(?<!V)v", perl = TRUE)[[1]][Team])%>%
-  select(!TeamAvTeamB)%>%
-  select(TeamName,FixtureKey,Team,X2PM,X2PA,X3PM,X3PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF)
-
-
-dfBoxScores2 <-dfBoxScores2%>%group_by(TeamName)%>%
-  summarise(X2PA = mean(X2PA))
-
-
-
-con <- dbConnect(RSQLite::SQLite(), "mydatabase.db")
-
-createTemp1Table <- "CREATE TEMP TABLE temp1 AS
+df_box_score_query <- "
+WITH TeamAvTeamB AS (
+    SELECT
+        FixtureKey,
+        SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12) AS TeamAvTeamB
+    FROM
+        box_scores
+)
 SELECT
-    FixtureKey,
-    SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12) AS TeamAvTeamB
+    TeamAvTeamB.FixtureKey AS FixtureKey, 
+    TRIM(CASE
+            WHEN Team = 1 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)
+            WHEN Team = 2 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
+            ELSE NULL -- Handle other cases if needed
+        END) AS TeamName,
+    Team,X2PM,X2PA,X3PM,X3PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF
 FROM
-    box_scores"
+    TeamAvTeamB
+    JOIN box_scores ON TeamAvTeamB.FixtureKey = box_scores.FixtureKey;"
 
-createTemp2Table <- "CREATE TEMP TABLE temp2 AS
-SELECT
-    FixtureKey,
-    TeamAvTeamB,
-    TRIM(SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)) AS TeamName
-FROM
-    temp1"
 
-joinTemp1Temp2 <- "SELECT
-    df.Team,df.X2PM, df.X2PA, df.X3PM,df.X3PA,df.FTM, df.FTA,df.ORB,
-    df.DRB,df.AST,df.STL,df.BLK,df.TOV,df.PF,t2.TeamName
-FROM
-    box_scores AS df
-JOIN
-    temp2 AS t2
-ON
-    df.FixtureKey = t2.FixtureKey"
+df_box_scores <- dbGetQuery(df_box_score_query, con)
+
+
+"1. **Shooting Percentages**:
+   - Calculate the Field Goal Percentage (FG%) for each team. FG% is the ratio of successful field goals (2PM + 3PM) to total field goal attempts (2PA + 3PA)."
+
+
+
+
+
+" - Calculate the Three-Point Percentage (3P%) for each team. 3P% is the ratio of successful three-pointers (3PM) to total three-point attempts (3PA).
+   - Calculate the Free Throw Percentage (FT%) for each team. FT% is the ratio of successful free throws (FTM) to total free throw attempts (FTA).
+
+2. **Rebound Differential**:
+   - Calculate the Offensive Rebound Differential (ORD) for each team. ORD is the difference between the average offensive rebounds (ORB) a team secures and the average offensive rebounds their opponents secure.
+   - Calculate the Defensive Rebound Differential (DRD) for each team. DRD is the difference between the average defensive rebounds (DRB) a team secures and the average defensive rebounds their opponents secure.
+
+3. **Assist-to-Turnover Ratio**:
+   - Calculate the Assist-to-Turnover Ratio (AST/TOV) for each team. This ratio measures a team's ball-handling efficiency. It's the ratio of assists (AST) to turnovers (TOV).
+
+4. **Steal and Block Averages**:
+   - Calculate the average number of steals (STL) and blocks (BLK) for each team. These metrics can represent a team's defensive capabilities.
+
+5. **Foul Differential**:
+   - Calculate the average difference in the number of fouls committed (PF) between a team and its opponents. This can indicate a team's discipline on the court.
+
+6. **Historical Performance**:
+   - Consider incorporating historical performance metrics. For example, calculate the team's win-loss record over a certain number of previous games.
+
+7. **Home Court Advantage**:
+   - If applicable, include a binary feature that indicates whether the game was played at the home court of one of the teams. Home court advantage can significantly impact game outcomes.
+
+8. **Day of the Week**:
+   - Extract the day of the week from the date and include it as a categorical feature. Some teams may perform differently on specific days.
+
+9. **Opponent Strength**:
+   - Consider including a feature that measures the strength of the opponent. This can be based on the opponent's win-loss record, rankings, or other relevant metrics.
+
+10. **Time Since Last Game**:
+    - Calculate the number of days since each team's last game. Fatigue can play a role in performance."
+
+
 
 
 

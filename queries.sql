49efd3864f251068be541432bd9da975e749ab76
Modith Hadya <40522163+PHNX-MOD@users.noreply.github.com>
2023-10-07 03:52:34 -0700

    updated the ORB, CTE statements in the query

--- queries.sql
+++ queries.sql
@@ -1,22 +1,59 @@
 WITH TeamAvTeamB AS (
     SELECT
-        FixtureKey,
-        SUBSTR(FixtureKey, 1, LENGTH(FixtureKey) - 12) AS TeamAvTeamB
+        bs.FixtureKey,
+        SUBSTR(bs.FixtureKey, 1, LENGTH(bs.FixtureKey) - 12) AS TeamAvTeamB  -- deleting the date from the fixture and temp column TeamAvTeamB
     FROM
-        box_scores
+        box_scores AS bs --bs as alias due to ambiguous column name: FixtureKey
 )
-SELECT DISTINCT
-    TeamAvTeamB.FixtureKey AS FixtureKey, 
-    TRIM(CASE
-            WHEN Team = 1 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)
-            WHEN Team = 2 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
-            ELSE NULL -- Handle other cases if needed
-        END) AS TeamName,
-    Team, X2PM, X2PA, X3PM, X3PA, FTM, FTA, ORB, DRB, AST, STL, BLK,TOV,
-    PF,
-    ROUND((CAST(X2PM AS REAL) + CAST(X3PM AS REAL)) / (CAST(X2PA AS REAL) + CAST(X3PA AS REAL))*100,2) AS 'FG%',
-    ROUND((CAST(X2PM AS REAL) / CAST(X3PA AS REAL))*100,2)  AS'3P%',
-    ROUND((CAST(FTM AS REAL) / CAST(FTA AS REAL))*100,2)  AS'FT%'
+
+-- Query 1 as a CTE, this helps to get a group by Teams to get the average of ORB and other columns then later helps to join in the final query
+, Query1 AS (
+    SELECT DISTINCT
+        TRIM(CASE
+                WHEN bs.Team = 1 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1) 
+                WHEN bs.Team = 2 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
+                ELSE NULL
+            END) AS TeamName,  -- extracting the teams from the TeamAvTeamB based on the TEAM column if TEAM is 1 then the first substring or else the other, then trimming the trailing and leading spaces 
+        AVG(bs.ORB) AS ORBAvg
+    FROM
+        TeamAvTeamB AS ta
+    JOIN box_scores AS bs ON ta.FixtureKey = bs.FixtureKey
+    GROUP BY
+        TeamName
+)
+
+-- Query 2 as a CTE, this helps to manipulate the main table 
+, Query2 AS (
+    SELECT
+        bs.FixtureKey,
+        TRIM(CASE
+                WHEN bs.Team = 1 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)
+                WHEN bs.Team = 2 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
+                ELSE NULL
+            END) AS TeamName,
+        TRIM(CASE
+                WHEN bs.Team = 2 THEN SUBSTR(TeamAvTeamB, 1, INSTR(TeamAvTeamB, 'v') - 1)
+                WHEN bs.Team = 1 THEN SUBSTR(TeamAvTeamB, INSTR(TeamAvTeamB, 'v') + 1)
+                ELSE NULL
+            END) AS Oppnent,  -- extracting the opponents from the TeamAvTeamB based on the TEAM column if TEAM is 1 then the first substring or else the other, then trimming the trailing and leading spaces 
+            
+        bs.Team, bs.X2PM, bs.X2PA, bs.X3PM, bs.X3PA, bs.FTM, bs.FTA, bs.ORB, bs.DRB, bs.AST, bs.STL, bs.BLK, bs.TOV,
+        bs.PF,
+        ROUND((CAST(bs.X2PM AS REAL) + CAST(bs.X3PM AS REAL)) / (CAST(bs.X2PA AS REAL) + CAST(bs.X3PA AS REAL))*100,2) AS 'FG%',
+        ROUND((CAST(bs.X2PM AS REAL) / CAST(bs.X3PA AS REAL))*100,2)  AS '3P%',
+        ROUND((CAST(bs.FTM AS REAL) / CAST(bs.FTA AS REAL))*100,2)  AS 'FT%'
+    FROM
+        TeamAvTeamB AS ta
+    JOIN box_scores AS bs ON ta.FixtureKey = bs.FixtureKey
+)
+
+-- Final Query joining Query1 and Query2 using TeamName
+SELECT
+    Query2.FixtureKey, Query2.TeamName,Query2.Oppnent, Query2.Team,Query2.X2PM, Query2.X2PA, Query2.X3PM, Query2.X3PA,Query2.FTM, Query2.FTA,
+    Query2.ORB,Query2.DRB,Query2.AST,Query2.STL,Query2.BLK,Query2.TOV, Query2.PF,Query2.'FG%', Query2.'3P%',Query2.'FT%',
+    ROUND(Query1.ORBAvg, 2) AS ORBAvg,
+    ROUND(Query3.ORBAvg, 2 ) AS OppORBAvg 
 FROM
-    TeamAvTeamB
-    JOIN box_scores ON TeamAvTeamB.FixtureKey = box_scores.FixtureKey;
+    Query1
+JOIN Query2 ON Query1.TeamName = Query2.TeamName
+JOIN Query1 AS Query3 ON Query2.Oppnent = Query3.TeamName;
